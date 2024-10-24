f1915eab86988244a9ad26f4fa972cf0e2228c55
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-10-24 15:01:56 +0100

    Updated header

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,45 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
 import AboutMe from "./AboutMe";
 import Contact from "./Contact";
 import Header from "./Header";
 import MyProjects from "./MyProjects";
 import Skills from "./Skills";
+import NavBar from "./NavBar";
 
 function App() {
+    const [headerText, setHeaderText] = useState("Hey, I'm Zainab. I'm a Software Developer");
+    const [displayedText, setDisplayedText] = useState("");
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+      const typingInterval = setInterval(() => {
+        if (index < headerText.length) {
+          setDisplayedText(
+            (currentText) => currentText + headerText.charAt(index)
+          );
+          setIndex(index + 1);
+        }
+      }, 100);
+
+      return () => clearInterval(typingInterval);
+    }, [index]); 
+
   return (
     <div className="container">
-      <nav className="navigation-bar">
-        <div className="nav-buttons">
-            <a href="#header" id="nav-name">
-              Zainab Choudhry
-            </a>
-            <a href="#header" id="nav-initials">
-              ZC
-            </a>
-          <a className="nav-links" href="#about-section">
-            About
-          </a>
-          <a className="nav-links" href="#projects-section">
-            Projects
-          </a>
-          <a className="nav-links" href="#skills-section">
-            Skills
-          </a>
-          <a className="nav-links" href="#contact-section">
-            Contact
-          </a>
-        </div>
-      </nav>
+      <NavBar />
       <main>
         <div className="overflow-container">
           <section className="title-section">
             <div className="title">
-              <h1>Welcome to my Portfolio</h1>
+              {/* <h1>
+                <span id="typed-text">{displayedText}</span>
+                <span className="cursor">|</span>
+              </h1> */}
+              <h1 className="first-line">Welcome to my Portfolio.</h1>
+              {/* <h1 className="second-line">
+                I'm a <span>Full Stack</span> Developer
+                <div className="text-underline"></div>
+              </h1> */}
               <p>Explore my latest projects and learn more about me.</p>
               <a href="#about-section">
                 <button>Learn More</button>
@@ -54,4 +59,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
---
+++ src/NavBar.jsx
@@ -0,0 +1,29 @@
+
+const NavBar = () => {
+    return (
+        <nav className="navigation-bar">
+        <div className="nav-buttons">
+          <a href="#header" id="nav-name">
+            Zainab Choudhry
+          </a>
+          <a href="#navigation-bar" id="nav-initials">
+            ZC
+          </a>
+          <a className="nav-links" href="#about-section">
+            About
+          </a>
+          <a className="nav-links" href="#projects-section">
+            Projects
+          </a>
+          <a className="nav-links" href="#skills-section">
+            Skills
+          </a>
+          <a className="nav-links" href="#contact-section">
+            Contact
+          </a>
+        </div>
+      </nav>
+    )
+};
+
+export default NavBar;
