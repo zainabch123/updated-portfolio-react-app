7049a81d22fbb02fe13e32085f48d1fbe638369f
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-10-31 11:35:22 +0000

    Updated about me

--- src/AboutMe/AboutMe.jsx
+++ src/AboutMe/AboutMe.jsx
@@ -10,19 +10,19 @@ export default function AboutMe() {
           <h2>About Me</h2>
           <p>
             Hi, I'm Zainab! I'm a full-stack developer and a recent graduate of
-            an intensive 6-month Software Development Bootcamp. Formerly
-            history teacher, my interest in technology began during COVID when I
-            had to adapt to teaching online and was introduced to a range of new
-            tools as a result. This experience sparked my curiosity about how
-            these technologies functioned, leading me to take the plunge and
-            enroll in a coding course.
+            an intensive 6-month Software Development Bootcamp. Formerly history
+            teacher, my interest in technology began during COVID when I had to
+            adapt to teaching online and was introduced to a range of new tools
+            as a result. This experience sparked my curiosity about how these
+            technologies functioned, leading me to take the plunge and enroll in
+            a coding course.
           </p>
           <br />
           <p>
-            Now a full-stack developer with over 900 hours of training in modern,
-            industry-relevant tech, coupled with my developed interpersonal,
-            critical thinking, and team building skills from my time in
-            teaching, I am eager to kickstart a career in Software
+            Now a full-stack developer with over 900 hours of training in
+            modern, industry-relevant tech, coupled with my developed
+            interpersonal, critical thinking, and team building skills from my
+            time in teaching, I am eager to kickstart a career in Software
             Development.
           </p>
         </div>
