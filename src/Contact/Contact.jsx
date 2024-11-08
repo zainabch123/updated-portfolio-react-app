9d7233faf86dd082bd8e7e06b58b2d1c343c1808
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-11-08 15:37:36 +0000

    Updated contact form handleSubmit and handleInput

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -21,7 +21,6 @@ export default function Contact() {
   }
 
   const handleOnSubmit = (event) => {
-      event.preventDefault();
     setFormData({
       Name: "",
       Email: "",
@@ -44,7 +43,6 @@ export default function Contact() {
         ref={formRef}
         action="https://public.herotofu.com/v1/1ddc7270-9619-11ef-840d-9fdbf4f87a8b"
         method="POST"
-        onSubmit={handleOnSubmit}
         className="animated-section"
       >
         <input
@@ -53,8 +51,6 @@ export default function Contact() {
           name="Name"
           id="name"
           required
-          onChange={handleOnInput}
-          value={formData.Name}
         />
         <input
           type="email"
@@ -62,15 +58,11 @@ export default function Contact() {
           name="Email"
           id="email"
           required
-          onChange={handleOnInput}
-          value={formData.Email}
         />
         <textarea
           id="message"
           placeholder="Message"
           name="Message"
-          onChange={handleOnInput}
-          value={formData.Message}
         ></textarea>
         <button id="submit-button" type="submit">
           Submit
